81febf89b469e1ef05c1f57a6fa1ac43f53aad8d
rakmate98 <50734395+rakmate9812@users.noreply.github.com>
2023-03-05 10:56:25 +0100

    creating-config.cs

---
+++ ManagementAPICalls/config.cs
@@ -0,0 +1,23 @@
+
+namespace cfg
+{
+    class Config
+    {
+        // defining config getters for the Program
+        public string CLIENT_ID
+        {
+            get { return "PASTE_YOUR_CLIENT_ID"; }
+        }
+
+        public string CLIENT_SECRET
+        {
+            get { return "PASTE_YOUR_CLIENT_SECRET"; }
+        }
+
+
+        public string TENANT_ID
+        {
+            get { return "PASTE_YOUR_TENANT_ID"; }
+        }
+    }
+}
